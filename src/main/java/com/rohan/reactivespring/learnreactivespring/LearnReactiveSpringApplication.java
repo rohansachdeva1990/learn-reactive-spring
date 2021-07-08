80c19db870e52f079e226d9f8fa6b99114c78679
Rohan Sachdeva <rohan.sachdeva1990@gmail.com>
2021-07-09 00:26:34 +0200

    project setup

---
+++ src/main/java/com/rohan/reactivespring/learnreactivespring/LearnReactiveSpringApplication.java
@@ -0,0 +1,13 @@
+package com.rohan.reactivespring.learnreactivespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnReactiveSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnReactiveSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohan/reactivespring/learnreactivespring/LearnReactiveSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohan.reactivespring.learnreactivespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnReactiveSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
